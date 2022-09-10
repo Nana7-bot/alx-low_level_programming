8bcdbf47e8681d059f7f8564d2947f9e9476b623
Michael Adams <nanaadams246@gmail.com>
2022-09-09 19:54:35 -0700

    Correction-1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase,
+ * followed by a new line
+ * Return: 0 if compiled well
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
+
