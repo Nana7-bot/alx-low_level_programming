1ca22c3edaa6aaee92128ba1815b3ee83aa6c3ee
Michael Adams <nanaadams246@gmail.com>
2022-09-14 08:31:10 -0700

    Correction-5

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
 # include <stdio.h>
 
 void positive_or_negative(int i);
@@ -5,3 +7,4 @@ int largest_number(int, int, int);
 void print_remaining_days(int, int, int);
 int convert_day(int, int);
 
+#endif 
