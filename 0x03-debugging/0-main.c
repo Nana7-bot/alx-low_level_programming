1bbc3680b016a808bf669d3c758203edf1e422aa
Michael Adams <nanaadams246@gmail.com>
2022-09-15 11:29:24 -0700

    Coorection-5 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* main - prints if integer is positive or negative
+* main - test function that prints if integer is positive or negative
 * Return: 0 if compiled well
 */
 
