7456c24afcf44133afb43f29af79b4ce468b108c
Michael Adams <nanaadams246@gmail.com>
2022-09-15 10:39:13 -0700

    Correction-1 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @m: number of times the character _ should be printed
+ */
+void print_line(int m)
+{
+	if (m <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		int i;
+
+		for (i = 1; i <= m; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+
+}
